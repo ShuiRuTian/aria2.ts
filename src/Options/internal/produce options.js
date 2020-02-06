c796d554e7b1c0f7183856f74c52ba934596c4da
Megrez <158983297@qq.com>
2020-02-06 22:49:58 +0800

    change options name to avoid useless midware.

--- src/Options/internal/produce options.js
+++ src/Options/internal/produce options.js
@@ -21,7 +21,8 @@ FunctionTrees.forEach((OptionTree)=>{
             classInformation.push("* "+comment);
         });
         classInformation.push("*/");
-        classInformation.push(optionProp+'?: '+optionValue+';');
+        // need to add char ', so char - could be supported directly. 
+        classInformation.push(`'${optionProp}'?: ${optionValue};`);
     });
     classInformation.push("\}");
     methodArray.push(classInformation.join("\n"));
@@ -36,18 +37,28 @@ FinalString = FinalString.replace(/--/g, "")
 FinalString = FinalString.replace(/=<(\w*)>;/g, "$1;")
 FinalString = FinalString.replace(/\[true\|false\];/g, "boolean;")
 
+
 // deal with some specail case
+FinalString = FinalString.replace(/'help\['\?: =<TAG>\|<KEYWORD>\];/, "help?: '#basic'| '#advanced'| '#http'| '#https'| '#ftp'| '#metalink'| '#bittorrent'| '#cookie'| '#hook'| '#file'| '#rpc'| '#checksum'| '#experimental'| '#deprecated'| '#help'| '#all';")
 
-//  help[: =<TAG>|<KEYWORD>];              help: "#basic"| "#advanced"| "#http"| "#https"| "#ftp"| "#metalink"| "#bittorrent"| "#cookie"| "#hook"| "#file"| "#rpc"| "#checksum"| "#experimental"| "#deprecated"| "#help"| "#all"
-//  checksum: =<TYPE>DIGEST;                checksum: string;
-//  stream_piece_selector: SELECTOR;            stream_piece_selector: "default"|"inorder"|"random"|"geom";
-//                                      uri_selector: "inorder"| "feedback" | "adaptive";
+FinalString = FinalString.replace(/'checksum'\?: =<TYPE>DIGEST;/, "'checksum'?: string;")
+FinalString = FinalString.replace(/'stream-piece-selector'\?: SELECTOR;/, "'stream-piece-selector'?: 'default'|'inorder'|'random'|'geom';")
+FinalString = FinalString.replace(/'uri-selector'\?: SELECTOR;/, "'uri-selector'?: 'inorder'| 'feedback' | 'adaptive';")
+FinalString = FinalString.replace(/'ssh-host-key-md'\?: =<TYPE>DIGEST;/, "'ssh-host-key-md'?: string;")
 
-/**
- * Or you can replace regular expression by using VsCode!
- * (interface.*)-(.*\{)     =>      $1_$2       repeat 4 times
- * -(.*;)    =>     _$1         repeat 7 times.
- * _    =>   
- * =<(\w*)>;     =>  $1;
- * \[true\|false\];      =>      boolean;
- */
+FinalString = FinalString.replace(/'select-file'\?: =<INDEX>...;/, "'select-file'?: string;")
+FinalString = FinalString.replace(/'bt-exclude-tracker'\?: =<URI>\[,...\];/, "'bt-exclude-tracker'?: string")
+FinalString = FinalString.replace(/'bt-min-crypto-level'\?: =plain\|arc4;/, "'bt-min-crypto-level'?: 'plain'|'arc4';")
+FinalString = FinalString.replace(/'bt-prioritize-piece'\?: =head\[=<SIZE>\],tail\[=<SIZE>\];/, "'bt-prioritize-piece'?: string;")
+FinalString = FinalString.replace(/'follow-metalink'\?: =true\|false\|mem;/, "'follow-metalink'?: true|false|'mem';")
+
+FinalString = FinalString.replace(/'metalink-location'\?: =<LOCATION>\[,...\];/, "'metalink-location'?: string;")
+FinalString = FinalString.replace(/'async-dns-server'\?: =<IPADDRESS>\[,...\];/, "'async-dns-server'?: string")
+FinalString = FinalString.replace(/'optimize-concurrent-downloads'\?:  \[true\|false\|<A>:<B>\];/, "'optimize-concurrent-downloads'?:  true|false|string;")
+FinalString = FinalString.replace(/'bt-tracker'\?: =<URI>\[,...\];/, "'bt-tracker'?: string;")
+FinalString = FinalString.replace(/'dht-entry-point'\?: =<HOST>:<PORT>;/, "'dht-entry-point'?: string;")
+FinalString = FinalString.replace(/'dht-entry-point6'\?: =<HOST>:<PORT>;/, "'dht-entry-point6'?: string;")
+FinalString = FinalString.replace(/'dht-listen-port'\?: =<PORT>...;/, "'dht-listen-port'?: string;")
+FinalString = FinalString.replace(/'index-out'\?: =<INDEX>PATH;/, "'index-out'?: string;")
+FinalString = FinalString.replace(/'listen-port'\?: =<PORT>...;/, "'listen-port'?: string;")
+FinalString = FinalString.replace(/'follow-torrent'\?: =true\|false\|mem;/, "'follow-torrent'?: true|false|'mem';")
